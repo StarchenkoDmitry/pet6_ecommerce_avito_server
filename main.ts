10530a4164900e54b9ced87401fe01c7145d6db9
StarchenkoDmitry <starchenkodmitrypavlovich@gmail.com>
2024-02-10 08:57:34 +0300

    refactor main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
 import { Server, Socket } from "socket.io";
-import { db } from "./src/db";
+import MyEmiter from "./src/utils/MyEmiter";
+
+import { IsChatId } from "./src/utils/chack";
+
 import { 
     CHAT_ADDED_MESSAGE,
     CHAT_ADD_MESSAGE,
+    CHAT_DELETED_MESSAGE,
     CHAT_DELETE_MESSAGE,
     CHAT_INIT, 
     CHAT_INIT_ERROR, 
@@ -11,11 +15,18 @@ import {
     EVENT_SERVER_MESSAGE, 
     ServerMessage 
 } from "./src/interfaces/chat";
-import MyEmiter from "./src/utils/MyEmiter";
-import { CHAT_EVENT_ADDED_MESSAGE, CHAT_EVENT_DELETED_MESSAGE, ChatEventCallback } from "./src/interfaces/chatevent";
+import { 
+    CHAT_EVENT_ADDED_MESSAGE, 
+    CHAT_EVENT_DELETED_MESSAGE, 
+    ChatEventCallback 
+} from "./src/interfaces/chatevent";
 import { findUserByToken } from "./src/db/user";
-import { createMessage, deleteMessage, findChatByIdAndUserId } from "./src/db/chat";
-import { IsChatId } from "./src/utils/chack";
+import { 
+    createMessage, 
+    deleteMessage, 
+    findChatByIdAndUserId 
+} from "./src/db/chat";
+import { getMessagesByChatId } from "./src/db/message";
 
 
 type Client = {
@@ -39,99 +50,106 @@ io.on("connection",Connect);
 
 
 async function Connect(socket:Socket){
-    const query = socket.handshake.query;
-    const token: string = socket.handshake.auth.token;
-    console.log("Connect token",token);
+    try {
+        const query = socket.handshake.query;
+        const token: string = socket.handshake.auth.token;
+        console.log("Connect token",token);
 
-    const chatId = query.chatId as string;
+        const chatId = query.chatId as string;
 
-    //validation chatId
-    if(!IsChatId(chatId)){
-        const mes: ServerMessage = {
-            type:CHAT_INIT_ERROR,
-            data:{ error:"invalid chatId" }
-        } 
-        socket.emit(EVENT_SERVER_MESSAGE,mes);
-        socket.disconnect();
-        return;
-    }
-
-    const user = await findUserByToken(token);
-
-    if(!user){
-        const mes: ServerMessage = {
-            type:CHAT_INIT_ERROR,
-            data:{ error:"unauthorized" }
-        } 
-        socket.emit(EVENT_SERVER_MESSAGE,mes);
-        socket.disconnect();
-        return;
-    }
-
-    const userId = user.id;
-
-    const chat = await findChatByIdAndUserId(chatId,userId);
-
-    if(!chat){
-        const mes: ServerMessage = {
-            type:CHAT_INIT_ERROR,
-            data:{ error:"hat is not exist"}
-        } 
-        socket.emit(EVENT_SERVER_MESSAGE,mes);
-        socket.disconnect();
-        return;
-    }
-    
-    const messages = await db.message.findMany({
-        where:{
-            chatId:chatId
-        },
-        orderBy:{
-            ceatedAt:"desc"
-        },
-        take:10
-    });
-
-    const mes: ServerMessage = {
-        type:CHAT_INIT,
-        data:{messages}
-    }
-    socket.emit(EVENT_SERVER_MESSAGE,mes);
-
-
-    const release = chats.sub(chatId,(event)=>{
-        switch(event.type){
-            case CHAT_EVENT_ADDED_MESSAGE:{
-                const data: ServerMessage = {
-                    type:CHAT_ADDED_MESSAGE,
-                    data:{ message: event.data.message }
-                } 
-                socket.emit(EVENT_SERVER_MESSAGE,data);
-                break;
-            }
-            case CHAT_EVENT_DELETED_MESSAGE:{
-                
-                break;
-            }
+        //validation chatId
+        if(!IsChatId(chatId)){
+            const mes: ServerMessage = {
+                type:CHAT_INIT_ERROR,
+                data:{ error:"invalid chatId" }
+            } 
+            socket.emit(EVENT_SERVER_MESSAGE,mes);
+            socket.disconnect();
+            return;
         }
-    });
 
+        const user = await findUserByToken(token);
 
-    const client: Client = {
-        socket,
-        chatId,
-        userId,
-        chatSub:release,
-    };
-
-    socket.on(EVENT_CLIENT_MESSAGE,(message)=>handlerClient(client,message));
-
-    socket.on("disconnect",()=>{
-        console.log("User disconnect: ",client.userId)
-        if(client.chatSub){
-            client.chatSub();
+        if(!user){
+            const mes: ServerMessage = {
+                type:CHAT_INIT_ERROR,
+                data:{ error:"unauthorized" }
+            } 
+            socket.emit(EVENT_SERVER_MESSAGE,mes);
+            socket.disconnect();
+            return;
         }
-    });
+
+        const userId = user.id;
+
+        const chat = await findChatByIdAndUserId(chatId,userId);
+
+        if(!chat){
+            const mes: ServerMessage = {
+                type:CHAT_INIT_ERROR,
+                data:{ error:"hat is not exist"}
+            } 
+            socket.emit(EVENT_SERVER_MESSAGE,mes);
+            socket.disconnect();
+            return;
+        }
+        
+        const messages = await getMessagesByChatId(chatId);
+
+        if(!messages){
+            console.log("Connect error: messages is not exist");
+            socket.disconnect();
+            return;
+        }
+
+        const mes: ServerMessage = {
+            type:CHAT_INIT,
+            data:{messages}
+        }
+        socket.emit(EVENT_SERVER_MESSAGE,mes);
+
+
+        const release = chats.sub(chatId,(event)=>{
+            switch(event.type){
+                case CHAT_EVENT_ADDED_MESSAGE:{
+                    const data: ServerMessage = {
+                        type:CHAT_ADDED_MESSAGE,
+                        data:{ message: event.data.message }
+                    }
+                    socket.emit(EVENT_SERVER_MESSAGE,data);
+                    break;
+                }
+                case CHAT_EVENT_DELETED_MESSAGE:{
+                    const data: ServerMessage = {
+                        type: CHAT_DELETED_MESSAGE,
+                        data:{ id: event.data.id }
+                    }
+                    socket.emit(EVENT_SERVER_MESSAGE,data);
+                    break;
+                }
+            }
+        });
+
+
+        const client: Client = {
+            socket,
+            chatId,
+            userId,
+            chatSub:release,
+        };
+
+        socket.on(EVENT_CLIENT_MESSAGE,(message)=>handlerClient(client,message));
+
+        socket.on("disconnect",()=>{
+            console.log("User disconnect: ",client.userId)
+            if(client.chatSub){
+                client.chatSub();
+            }
+        });
+    } catch (error) {
+        console.log("Connect error:",error);
+        socket.disconnect();
+    }
 }
 
 async function handlerClient(client:Client,message:ClientMessage){
@@ -149,7 +167,12 @@ async function handlerClient(client:Client,message:ClientMessage){
                 break;
             }
             case CHAT_DELETE_MESSAGE:{
-                const deleted = await deleteMessage(client.chatId,client.userId,message.data.id)
+                const deleted = await deleteMessage(client.chatId,client.userId,message.data.id);
+                if(!deleted)return;
+                chats.emit(client.chatId,{
+                    type:CHAT_EVENT_DELETED_MESSAGE,
+                    data:{ id: message.data.id }
+                });
                 break;
             }
         }
